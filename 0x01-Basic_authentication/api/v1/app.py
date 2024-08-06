81447b7561df03a0e707bdc3d3415b74da3aaf6e
Ephyz8 <ziwoyaephraim@gmail.com>
2024-08-06 15:18:42 +0000

    unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,7 +22,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error) -> Tuple[str, Literal[401]]:
+def unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Unauthorized"}), 401
